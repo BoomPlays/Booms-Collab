e96a64f1f0e0a014ce38806cdb1c9f9d66666b5c
BoomPlays <leon.bugeja.anna@gmail.com>
2018-04-07 19:48:48 +0200

    first commit

---
+++ killwor.py
@@ -0,0 +1,14 @@
+import discord
+import asyncio
+import async_timeout
+import traceback
+import time
+import datetime
+import os
+import json
+import aiohttp
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='-')
+
+client.run('NDMyMjI0NjQzMjY5Nzg3Njc4.DaqMOw.E7KRZ6-cqEg8bNcYTu0Q2SSOfSQ')
